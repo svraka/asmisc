d146e2d96dd7a214c23dfcce5dedf4243fb4d0f1
Andras Svraka <svraka.andras@gmail.com>
2019-02-26 15:29:29 +0100

    Add df_sizes function

--- R/utils.R
+++ R/utils.R
@@ -85,3 +85,19 @@ needed_project_packages <- function(path = ".") {
     to_install
   }
 }
+
+#' List data frames and their sizes
+#'
+#' List data frames in global environment and their sizes in megabytes.
+#'
+#' @return
+#' A tibble, arranged in decreasing order of size.
+#'
+#' @export
+df_sizes <- function() {
+  dfs <- names(which(sapply(.GlobalEnv, is.data.frame)))
+  sapply(dfs, function(x) utils::object.size(get(x))) %>%
+    tibble::enframe(name = "df", value = "size") %>%
+    dplyr::arrange(dplyr::desc(.data$size)) %>%
+    dplyr::mutate(size = as.integer(round(.data$size / 2024^2, 0)))
+}
