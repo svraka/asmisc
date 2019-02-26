a6b72e3d8d113292b42b79b5b1136580681e0d20
Andras Svraka <svraka.andras@gmail.com>
2019-02-26 14:33:55 +0100

    Functions to find loaded packages in a non-package project

--- R/utils.R
+++ R/utils.R
@@ -32,3 +32,56 @@ chr_values <- function(x) {
     stringr::str_subset("[^\\d]") %>%
     utils::head(n = 10)
 }
+
+#' List packages used in a project
+#'
+#' Find packages loaded in \code{\link{library}}, or \code{\link{require}} calls
+#' in a non-package project.
+#'
+#' @param path Relative, or absolute path to a project.
+#'
+#' @return
+#' A character vector with the names of packges loaded.
+#'
+#' @details
+#' These functions look for \code{\link{library}}, or \code{\link{require}}
+#' calls in \code{*.R}, \code{*.Rmd}, \code{*.Rnw}, \code{*.Rtex}, and
+#' \code{*.Rrst} files.  \code{needed_project_packages} list all pacakges that
+#' are not installed.
+#'
+#' @export
+find_project_packages <- function(path = ".") {
+  files <- list.files(
+    path = path,
+    pattern = ".+\\.R[(md)(nw)(tex)(rst)]*$",
+    recursive = TRUE,
+    full.names = TRUE
+  )
+
+  list_packages <- function(file) {
+    readLines(file) %>%
+      grep("library|require\\(", ., value = TRUE, perl = TRUE) %>%
+      gsub(".*(library|require)\\((.+?)\\).*", "\\2", ., perl = TRUE) %>%
+      unique %>% sort
+  }
+
+  lapply(files, list_packages) %>%
+    unlist %>% unique %>% sort
+}
+
+#' @describeIn find_project_packages Only list packages that are not installed.
+#' @export
+needed_project_packages <- function(path = ".") {
+  project_packages <- find_project_packages(path)
+
+  installed_packages <- row.names(utils::installed.packages())
+
+  to_install <- setdiff(project_packages, installed_packages)
+
+  if (identical(to_install, character(0))) {
+    message("All packages in the project are installed.")
+  } else {
+    message("You need to install some packages:")
+    to_install
+  }
+}
