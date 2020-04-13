08c145c1393451b449892b7fa46424efc7604e7b
András Svraka <svraka.andras@gmail.com>
2020-04-13 19:17:30 +0200

    Document `metadata_parquet()`

--- R/arrow.R
+++ R/arrow.R
@@ -1,3 +1,34 @@
+#' Query metadata of a Parquet file
+#'
+#' Query column names and column types of an Apache Parquet data file.
+#'
+#' @param file Path to a Parquet file
+#'
+#' @return
+#' A data frame with two columns:
+#'
+#' \describe{
+#'   \item{\code{name}}{Column names}
+#'   \item{\code{type}}{Column types, as returned by the
+#'   \code{\link[arrow:ParquetFileReader]{ParquetFileReader$GetSchema()}}
+#'   method. This does not directly correspond to navtive R data types
+#'   but should be informative nevertheless.}
+#' }
+#'
+#' If the \pkg{tibble} package is available, the data frame is
+#' converted into a tibble.
+#'
+#' @author András Svraka
+#'
+#' @seealso \code{\link[arrow:write_parquet]{write_parquet}},
+#'   \code{\link[arrow:read_parquet]{read_parquet}}
+#'
+#' @examples
+#' tmp <- tempfile()
+#' arrow::write_parquet(iris, tmp)
+#' metadata_parquet(tmp)
+#'
+#' @export
 metadata_parquet <- function(file) {
   pq <- arrow::ParquetFileReader$create(file)
 
