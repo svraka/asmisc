19b3882f72dd6d8a6dd9bde1d23c3333b14e098a
András Svraka <svraka.andras@gmail.com>
2020-10-01 10:22:40 +0200

    Fix test failure on CRAN locale

--- tests/testthat/test-codebook.R
+++ tests/testthat/test-codebook.R
@@ -1,14 +1,13 @@
 context("Codebook")
 
 test_that("all default skimmers are covered", {
-  default_skimmers <- names(skimr::get_default_skimmer_names())
+  default_skimmers <- sort(names(skimr::get_default_skimmer_names()))
   # This list is manually copied from `codebook()`. See comments why
   # the custom sfl cannot be defined. `integer` is left out, as skimr
   # treats them as numerics.
-  codebook_skimmers <- c("AsIs", "character", "complex", "Date",
-                         "difftime", "factor", "list",
-                         "logical", "numeric", "POSIXct", "Timespan",
-                         "ts")
+  codebook_skimmers <- sort(c("AsIs", "character", "complex", "Date",
+                              "difftime", "factor", "list", "logical",
+                              "numeric", "POSIXct", "Timespan", "ts"))
 
   expect_identical(default_skimmers, codebook_skimmers)
 })
