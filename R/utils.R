626787f0c859435b2b4488db3044e9d8e2e7b1c2
András Svraka <svraka.andras@gmail.com>
2024-09-11 11:08:07 +0200

    find_project_packages: Drop function
    No longer needed with projects using targets and renv

--- R/utils.R
+++ R/utils.R
@@ -3,58 +3,6 @@ tlmgr_install_svraka_pkgs <- function() {
   tinytex::tlmgr_install(pkgs)
 }
 
-#' List packages used in a project
-#'
-#' Find packages loaded in \code{\link{library}}, or \code{\link{require}} calls
-#' in a non-package project.
-#'
-#' @param path Relative, or absolute path to a project.
-#' @param not_installed Only list packages that are not installed.
-#'
-#' @return
-#' A character vector with the names of packges loaded.
-#'
-#' @details
-#' This function looks for \code{\link{library}}, or \code{\link{require}}
-#' calls in \code{*.R}, \code{*.Rmd}, \code{*.Rnw}, \code{*.Rtex}, and
-#' \code{*.Rrst} files.
-#'
-#' @export
-find_project_packages <- function(path = ".", not_installed = FALSE) {
-  files <- list.files(
-    path = path,
-    pattern = ".+\\.R[(md)(nw)(tex)(rst)]*$",
-    recursive = TRUE,
-    full.names = TRUE
-  )
-
-  list_packages <- function(file) {
-    x <- readLines(file)
-    x <- grep("library|require\\(", x, value = TRUE, perl = TRUE)
-    x <- gsub(".*(library|require)\\((.+?)\\).*", "\\2", x, perl = TRUE)
-    x <- sort(unique(x))
-
-    x
-  }
-
-  project_packages <- lapply(files, list_packages)
-  project_packages <- sort(unique(unlist(project_packages)))
-
-  if (not_installed == TRUE) {
-    installed_packages <- row.names(utils::installed.packages())
-
-    to_install <- setdiff(project_packages, installed_packages)
-
-    if (identical(to_install, character(0))) {
-      message("All packages in the project are installed.")
-    } else {
-      message("You need to install some packages:")
-      to_install
-    }
-  } else {
-    project_packages
-  }
-}
 #' Pretty print date in several languages
 #'
 #' Format dates according to the conventions of several languages.
