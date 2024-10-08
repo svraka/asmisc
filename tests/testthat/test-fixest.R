bfdc2c2cae0976d321f666e7e0c555ecb0dc7c86
András Svraka <svraka.andras@gmail.com>
2024-10-08 13:45:51 +0200

    fixest: Add test for a list of models with mixed type

--- tests/testthat/test-fixest.R
+++ tests/testthat/test-fixest.R
@@ -14,6 +14,18 @@ test_that("Converting fixest to a nested tibble", {
   expect_identical(t, tibble::tibble(id = 1L, model = list(est_s)))
 })
 
+test_that("Converting a mixed list of `fixest` and `fixest_multi` objects to a nested tibble", {
+  l <- list(est_s, est_m)
+  lt <- lapply(l, tibble::as_tibble)
+  lt <- dplyr::bind_rows(lt)
+
+  expect_length(lt[["model"]], 3)
+  expect_identical(names(lt),
+                   # Column order depend on the order of list element,
+                   # it something we shouldn't touch.
+                   c("id",  "model", "lhs"))
+})
+
 test_that("glance_custom.fixest works in single fixest models", {
   expect_identical(modelsummary::glance_custom(est_s),
                    data.frame("Family" = "OLS"))
