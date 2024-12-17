9d0cf46efd76e5bee0052681350e0599ef09eb01
András Svraka <svraka.andras@gmail.com>
2024-12-17 18:07:23 +0100

    fixest: Add IV fit statistics to custom glance

--- R/fixest.R
+++ R/fixest.R
@@ -55,16 +55,40 @@ as_tibble.fixest <- function(x) {
 
 #' Custom glances for fixest modelsummaries
 #'
-#' Add model family to glances in fixest modelsummaries using the
+#' Add further glances to fixest modelsummaries using the
 #' [modelsummary::glance_custom()] customization interface.
 #'
 #' @inheritParams modelsummary::glance_custom
+#' @inheritParams fixest::fitstat
+#' @param ... Further arguments passed to [fixest::fitstat()]'s `...`
+#'   argument.
+#'
 #' @exportS3Method modelsummary::glance_custom
+#'
+#' @details
+#'
+#' By default [modelsummary::modelsummary()] adds a lot of useful
+#' information to glances (fixed effects, VCOV types) but omits some
+#' others which are often important, and [fixest::etable()] makes
+#' their reporting easy. We extend the custom glance with the
+#' following:
+#'
+#' - Family (OLS, Poisson, etc.)
+#' - IV fit statistics. Note, this is likely to require further
+#'   processing with a custom [`modelsummary::gof_map`] to clean up
+#'   names of statistics and which instrument they refer to. Due to
+#'   the way \pkg{modelsummary} handles `...`s, we cannot pass on
+#'   further arguments, therefore we include all but Wald tests. They
+#'   are omitted because their value depends on the VCOV type which,
+#'   cannot be passed on either. ([fixest::fitstat()] lists Wald and
+#'   DoF for t-tests (aka G) as two examples where VCOV is needed,
+#'   checking the source confirms that it is not used anywhere else.)
 glance_custom.fixest <- function(x, ...) {
   # modelsummary always extracts single models from `fixest_multi`
   # objects, thus. Just to make sure, we bail out if that is not the
   # case
   stopifnot(inherits(x, "fixest"))
+
   # fixest doesn't expose a function to convert model families into
   # readable labels, it's all done internally in `fixest::etable`. I
   # don't want to copy it (and fixest is licenced under GPL anyway, so
@@ -79,7 +103,25 @@ glance_custom.fixest <- function(x, ...) {
   # columns, where the first contains row labels, and the second
   # "stats".
   family <- t[t[[1]] == "Family", ][[2]]
+  family <- data.frame("Family" = family)
 
-  out <- data.frame("Family" = family)
+  if (is.null(x[["iv_stage"]])) {
+    # A zero-column data frame can always be column-bound.
+    iv_fitstats <- data.frame(matrix(ncol = 0, nrow = 1))
+  } else {
+    types <- c("ivf1", "ivf2", "wh", "sargan")
+    iv_fitstats <- fixest::fitstat(x, type = types)
+    # We drop `NA` values, as `NA`s cannot be converted to a data
+    # frame. Result can be `NA` for the Sargan test as it only makes
+    # sense if the number of endogenous regressors is less than the
+    # number of instruments.
+    iv_fitstats_keep <- sapply(iv_fitstats,
+                               function(x) length(x) == 1 && is.na(x))
+    iv_fitstats <- iv_fitstats[!iv_fitstats_keep]
+    iv_fitstats <- lapply(iv_fitstats, function(x) do.call(data.frame, x))
+    iv_fitstats <- do.call(cbind, args = iv_fitstats)
+  }
+
+  out <- cbind(family, iv_fitstats)
   return(out)
 }
--- tests/testthat/test-fixest.R
+++ tests/testthat/test-fixest.R
@@ -26,12 +26,7 @@ test_that("Converting a mixed list of `fixest` and `fixest_multi` objects to a n
                    c("id",  "model", "lhs"))
 })
 
-test_that("glance_custom.fixest works in single fixest models", {
-  expect_identical(modelsummary::glance_custom(est_s),
-                   data.frame("Family" = "OLS"))
-})
-
-test_that("We extended modelsummary GoFs", {
+test_that("GoFs on mulitple estimations", {
   ms_multi <- modelsummary::modelsummary(est_m, output = "modelsummary_list")
   glance_names <- unique(lapply(ms_multi, function(x) names(x[["glance"]])))
 
@@ -39,4 +34,39 @@ test_that("We extended modelsummary GoFs", {
   expect_identical(glance_names,
                    list(c("aic", "bic", "r.squared", "adj.r.squared",
                           "rmse", "nobs", "vcov.type", "Family")))
+})
+
+test_that("GoFs on IV models", {
+  base <- iris
+  names(base) <- c("y", "x1", "x_endo_1", "x_inst_1", "fe")
+  set.seed(2)
+  base$x_inst_2 <- 0.2 * base$y + 0.2 * base$x_endo_1 + rnorm(150, sd = 0.5)
+  base$x_endo_2 <- 0.2 * base$y - 0.2 * base$x_inst_1 + rnorm(150, sd = 0.5)
+
+  fml <-
+    list(iv1 = y ~ x1 | fe | x_endo_1 ~ x_inst_1,
+         iv2 = y ~ x1 | fe | x_endo_1 + x_endo_2 ~ x_inst_1 + x_inst_2)
+
+  m <- lapply(fml, function(x) fixest::feols(x, data = base))
+  ms <- lapply(m, function(x) modelsummary::modelsummary(x, output = "modelsummary_list"))
+  ms_gof_names <- lapply(ms, function(x) names(x[["glance"]]))
+
+  expect_identical(ms_gof_names[["iv1"]],
+                   c("aic", "bic", "r.squared", "adj.r.squared",
+                     "r2.within", "r2.within.adjusted", "rmse",
+                     "nobs", "FE: fe", "vcov.type", "Family",
+                     "ivf1::x_endo_1.stat", "ivf1::x_endo_1.p",
+                     "ivf1::x_endo_1.df1", "ivf1::x_endo_1.df2",
+                     "ivf2.stat", "ivf2.p", "ivf2.df1", "ivf2.df2",
+                     "wh.stat", "wh.p", "wh.df1", "wh.df2"))
+  expect_identical(ms_gof_names[["iv2"]],
+                   c("aic", "bic", "r.squared", "adj.r.squared",
+                     "r2.within", "r2.within.adjusted", "rmse",
+                     "nobs", "FE: fe", "vcov.type", "Family",
+                     "ivf1::x_endo_1.stat", "ivf1::x_endo_1.p",
+                     "ivf1::x_endo_1.df1", "ivf1::x_endo_1.df2",
+                     "ivf1::x_endo_2.stat", "ivf1::x_endo_2.p",
+                     "ivf1::x_endo_2.df1", "ivf1::x_endo_2.df2",
+                     "ivf2.stat", "ivf2.p", "ivf2.df1", "ivf2.df2",
+                     "wh.stat", "wh.p", "wh.df1", "wh.df2"))
 })
